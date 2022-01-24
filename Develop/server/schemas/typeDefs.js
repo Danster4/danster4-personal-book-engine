bcb34501133c24aecc775e622a317c5bfd3855be
Danster4 <86613317+Danster4@users.noreply.github.com>
2022-01-23 19:51:53 -0500

    added typeDefs in Schemas directory with query for me, and mutations for login, addUser, saveBook, and removeBook. Also began some front end changes to queries and mutations

---
+++ Develop/server/schemas/typeDefs.js
@@ -0,0 +1,51 @@
+// import the gql tagged template function
+const { gql } = require('apollo-server-express');
+
+// create our typeDefs
+const typeDefs = gql`
+  input BookInput {
+    authors: [String]
+    description: String!
+    bookId: ID!
+    image: String
+    link: String
+    title: String!
+  }
+
+  type Book {
+    bookId: String!
+    authors: [String]
+    description: String!
+    title: String!
+    image: String
+    link: String
+  }
+
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    me: User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(input: BookInput): User
+    removeBook(bookId: ID!): User
+  }
+`;
+
+
+// export the typeDefs
+module.exports = typeDefs;
